d6297f17bfe9f6132de4b2bfd58be35fd59607ad
Antostef <antoinedominique.stefanaggi@ynov.com>
2020-02-24 12:02:19 +0100

    first commit

---
+++ first_project/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	string nom;
+
+	cout << "Enter your name:";
+	cin >> nom
+
+	std::cout << "Hello "+nom << std::endl;
+}
